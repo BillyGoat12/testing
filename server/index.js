3aca01807d5e18405c7925290fed129c0d098709
Billy <nbilly319@gmail.com>
2021-01-20 18:07:11 -0600

    (update) files

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+
+const frontEnd = path.join(__dirname, '..', 'client', 'dist', 'index.html')
+
+const app = express()
+
+app.use(cors)
+app.use(express.json())
+
+app.use(express.static(frontEnd))
+
+app.get('/test', (req, res) => {
+    res.json('here')
+})
+
+app.listen(3000, () => {
+    console.info('http://localhost:3000')
+})
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'client', 'src', 'index.jsx'),
+    output: {
+      path: path.resolve(__dirname, 'client' ,'dist'),
+      filename: 'bundle.js'
+    },
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.(jsx|js)$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                      presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                  }
+            }
+        ],
+    }
+};
